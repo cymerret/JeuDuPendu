d50ccdd86d0c81f96ed142b70346c0a21862dd37
Cyril Merret <cymerret@gmail.com>
2024-12-18 17:25:42 +0100

    Ajoutez des fichiers projet.

---
+++ JeuDuPendu2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace JeuDuPendu2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMot = new System.Windows.Forms.TextBox();
+            this.grbMotMystere = new System.Windows.Forms.GroupBox();
+            this.lblMot = new System.Windows.Forms.Label();
+            this.grbLettres = new System.Windows.Forms.GroupBox();
+            this.pctRejouer = new System.Windows.Forms.PictureBox();
+            this.pctPendu = new System.Windows.Forms.PictureBox();
+            this.grbMotMystere.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRejouer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctPendu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMot
+            // 
+            this.txtMot.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMot.Location = new System.Drawing.Point(6, 19);
+            this.txtMot.Name = "txtMot";
+            this.txtMot.Size = new System.Drawing.Size(228, 31);
+            this.txtMot.TabIndex = 0;
+            this.txtMot.Visible = false;
+            this.txtMot.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMot_KeyPress);
+            // 
+            // grbMotMystere
+            // 
+            this.grbMotMystere.Controls.Add(this.lblMot);
+            this.grbMotMystere.Controls.Add(this.txtMot);
+            this.grbMotMystere.Location = new System.Drawing.Point(12, 12);
+            this.grbMotMystere.Name = "grbMotMystere";
+            this.grbMotMystere.Size = new System.Drawing.Size(241, 60);
+            this.grbMotMystere.TabIndex = 0;
+            this.grbMotMystere.TabStop = false;
+            this.grbMotMystere.Text = "Mot à chercher";
+            // 
+            // lblMot
+            // 
+            this.lblMot.AutoSize = true;
+            this.lblMot.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMot.Location = new System.Drawing.Point(6, 19);
+            this.lblMot.Name = "lblMot";
+            this.lblMot.Size = new System.Drawing.Size(0, 31);
+            this.lblMot.TabIndex = 3;
+            // 
+            // grbLettres
+            // 
+            this.grbLettres.Location = new System.Drawing.Point(12, 78);
+            this.grbLettres.Name = "grbLettres";
+            this.grbLettres.Size = new System.Drawing.Size(241, 117);
+            this.grbLettres.TabIndex = 1;
+            this.grbLettres.TabStop = false;
+            this.grbLettres.Text = "Lettres à tester";
+            // 
+            // pctRejouer
+            // 
+            this.pctRejouer.Image = global::JeuDuPendu2.Properties.Resources.playagain;
+            this.pctRejouer.Location = new System.Drawing.Point(98, 222);
+            this.pctRejouer.Name = "pctRejouer";
+            this.pctRejouer.Size = new System.Drawing.Size(46, 46);
+            this.pctRejouer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pctRejouer.TabIndex = 3;
+            this.pctRejouer.TabStop = false;
+            this.pctRejouer.Click += new System.EventHandler(this.pctRejouer_Click);
+            // 
+            // pctPendu
+            // 
+            this.pctPendu.Image = global::JeuDuPendu2.Properties.Resources.pendu0;
+            this.pctPendu.Location = new System.Drawing.Point(259, 18);
+            this.pctPendu.Name = "pctPendu";
+            this.pctPendu.Size = new System.Drawing.Size(250, 250);
+            this.pctPendu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pctPendu.TabIndex = 2;
+            this.pctPendu.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 282);
+            this.Controls.Add(this.pctRejouer);
+            this.Controls.Add(this.pctPendu);
+            this.Controls.Add(this.grbLettres);
+            this.Controls.Add(this.grbMotMystere);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grbMotMystere.ResumeLayout(false);
+            this.grbMotMystere.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRejouer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctPendu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbMotMystere;
+        private System.Windows.Forms.GroupBox grbLettres;
+        private System.Windows.Forms.PictureBox pctPendu;
+        private System.Windows.Forms.Label lblMot;
+        private System.Windows.Forms.PictureBox pctRejouer;
+    }
+}
+
---
+++ JeuDuPendu2/Form1.cs
@@ -0,0 +1,216 @@
+﻿/* Title : Jeu du pendu
+ * Author : Cyril Merret
+ * Date : 19/09/2024
+ * Upgrade : améliorations pour partie en plusieurs rounds
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JeuDuPendu2
+{
+    public partial class Form1 : Form
+    {
+        int essai = 0;
+        int erreur = 0;
+        string motMystere;
+        TextBox txtMot;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreaLettres();
+            ChangePhase();
+        }
+
+        /// <summary>
+        /// Création des boutons lettres
+        /// </summary>
+        private void CreaLettres()
+        {
+            int corX = 10;
+            int corY = 20;
+            for(int i = 0; i < 26; i++)
+            {
+                // création des boutons
+                Button btnLettre = new Button();
+                grbLettres.Controls.Add(btnLettre);
+                btnLettre.Location = new Point(corX, corY);
+                btnLettre.Size = new Size(23, 23);
+                btnLettre.Text = ((char)('A' + i)).ToString();
+                btnLettre.Click += new EventHandler(btnLettre_Click);
+
+                // positionnement des boutons
+                corX += 25;
+                if ((i + 1) % 9 == 0)
+                {
+                    corX = 10;
+                    corY += 25;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Changement de phases : phase de saisie du mot <-> phase de recherche du mot
+        /// </summary>
+        private void ChangePhase()
+        {
+            grbLettres.Enabled = !grbLettres.Enabled;
+            txtMot.Visible = !txtMot.Visible;
+            txtMot.Text = "";
+            pctRejouer.Visible = !pctRejouer.Visible;
+
+            // préparation pour phase de recherche du mot
+            if (txtMot.Visible == false)
+            {
+                for (int i = 0; i < motMystere.Length; i++)
+                {
+                    lblMot.Text += "-";
+                }
+            }
+            // préparation pour phase de saisie du mot
+            else
+            {
+                lblMot.Text = "";
+                txtMot.Focus();
+                essai = 0;
+                erreur = 0;
+                ChangeImage();
+                for (int lettre = 0; lettre < grbLettres.Controls.Count; lettre++)
+                {
+                    grbLettres.Controls[lettre].Enabled = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Test pour vérifier si la lettre choisie est dans le mot à trouver
+        /// </summary>
+        /// <param name="lettre"></param>
+        private void TestLettre(char lettre)
+        {
+            // recherche de la lettre dans le mot
+            bool isValide = false;
+            for (int i = 0; i < motMystere.Length; i++)
+            {
+                char x = motMystere[i];
+                // vérifie si la lettre est valide
+                if (motMystere[i].Equals(lettre))
+                {
+                    lblMot.Text = lblMot.Text.Remove(i, 1);
+                    lblMot.Text = lblMot.Text.Insert(i, lettre.ToString());
+                    isValide = true;
+                }
+            }
+            // vérifie si la lettre est invalide
+            if (!isValide)
+            {
+                erreur++;
+                ChangeImage();
+            }
+            // vérifie si une des conditions de fin de partie est atteinte
+            if (lblMot.Text.Equals(motMystere) || (erreur == 10))
+            {
+                FinPartie();
+            }
+        }
+
+        /// <summary>
+        /// Mise à jour de l'image du pendu
+        /// </summary>
+        private void ChangeImage()
+        {
+            string pathImage = "E:\\Programmes\\Visual Studio Entreprise 2019\\Projets\\B12SQ2\\JeuDuPendu2\\JeuDuPendu2\\Ressources\\pendu" + erreur + ".png";
+            pctPendu.Image = Image.FromFile(pathImage);
+        }
+
+        /// <summary>
+        /// Arrêt de la partie et affichage du résultat
+        /// </summary>
+        private void FinPartie()
+        {
+            string message;
+            if (erreur == 10)
+            {
+                message = "Perdu... \r\nLe mot à trouver était : " + motMystere;
+            }
+            else
+            {
+                message = "Bravo ! \r\nNombre d'essai : " + essai;
+            }
+            DialogResult conclusion = MessageBox.Show(message, "Partie terminée", MessageBoxButtons.OK);
+            if (conclusion == DialogResult.OK)
+            {
+                ChangePhase();
+            }
+        }
+
+        /// <summary>
+        /// Récupération de la lettre choisie
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLettre_Click(object sender, EventArgs e)
+        {
+            Button btnLettre = (Button)sender;
+            char lettre = char.Parse(((Button)sender).Text);
+            essai++;
+            btnLettre.Enabled = false;
+            TestLettre(lettre);
+        }
+        
+        /// <summary>
+        /// Passage à la phase de recherche du mot si le mot est valide
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtMot_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar == (char)Keys.Enter) && (txtMot.Text != ""))
+            {
+                bool isValide = true;
+                for (int i = 0; i < txtMot.Text.Length; i++)
+                {
+                    if (((char)txtMot.Text[i] < 65) || ((char)txtMot.Text[i] > 90 && (char)txtMot.Text[i] < 97) || ((char)txtMot.Text[i] > 122))
+                    {
+                        isValide = false;
+                    }
+                }
+                if (isValide)
+                {
+                    motMystere = txtMot.Text.ToUpper();
+                    ChangePhase();
+                }
+                else
+                {
+                    if (MessageBox.Show("Veillez à ne saisir que des lettres simples (pas d'accents, de caractères spéciaux ou de nombres)", "Erreur de saisie", MessageBoxButtons.OK) == DialogResult.OK)
+                    {
+                        txtMot.Text = "";
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Arrêt et relance d'une partie (non finie)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pctRejouer_Click(object sender, EventArgs e)
+        {
+            ChangePhase();
+        }
+    }
+}
